5c6afad105febb4ec98a3f152e907f15b3264085
Paul <35400575+musicEnthusiast03@users.noreply.github.com>
2021-07-29 10:27:44 -0400

    Add files via upload

--- public/savings.js
+++ public/savings.js
@@ -20,21 +20,43 @@ var firebaseConfig = {
   firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
 
-        firebase.database().ref("users").on("child_added", function(lists){
+        firebase.database().ref("Savings/" + 'JustSave/' + `${user.uid}/`).on("value", function(lists){
             lists.forEach(function(data){
                 var info = data.val()
                 var postkey = data.key
+                console.log(postkey)
                 document.getElementById("list_div").innerHTML += `
-                <div class="eachGoal" id="B">
+                <div class="eachGoal" id="${postkey}">
                 <div class="card bg-dark text-white">
                   <img src="https://www.macmillandictionary.com/external/slideshow/full/White_full.png" class="card-img" alt="..." height="300px">
                   <div class="overlay-div"></div>
                   <div class="card-img-overlay">
                     <h3 class="card-title">${info.goalNameOne}</h3>
                     <p class="card-text"></p>
-                    <p class="card-text">Last updated 3 mins ago</p>
                     <div class="progress">
-                      <progress class="progress is-success" value="${info.amtOne}" max="${info.objAmt}"></progress>
+                      <progress class="progress is-success" value="100" max="100" ></progress>
+                    </div>
+                  </div>
+                </div>
+              </div>
+                `
+            })
+        })
+        firebase.database().ref("Savings/" + 'ExactAmount/' + `${user.uid}/`).on("value", function(lists){
+            lists.forEach(function(data){
+                var info = data.val()
+                var postkey = data.key
+                console.log(postkey)
+                document.getElementById("list_div").innerHTML += `
+                <div class="eachGoal" id="${postkey}">
+                <div class="card bg-dark text-white">
+                  <img src="https://www.macmillandictionary.com/external/slideshow/full/White_full.png" class="card-img" alt="..." height="300px">
+                  <div class="overlay-div"></div>
+                  <div class="card-img-overlay">
+                    <h3 class="card-title">${info.goalNameOne}</h3>
+                    <p class="card-text"></p>
+                    <div class="progress">
+                      <progress class="progress is-success" value="100" max="100" ></progress>
                     </div>
                   </div>
                 </div>
@@ -44,14 +66,14 @@ var firebaseConfig = {
         })
 
         goalButton.addEventListener("click", function(){
-            firebase.database().ref('users/' + `${user.uid}/` + `${goalNameOne}`).set({
-                objAmt:objAmt,
+            firebase.database().ref('Savings/' + 'ExactAmount/' +`${user.uid}/` + `${goalNameOne}`).set({
+                objAmt:objAmt, 
                 amtOne:amtOne,
                 goalNameOne:goalNameOne
             })
         })
         casualButton.addEventListener("click", function(){
-            firebase.database().ref('users/' + `${user.uid}/` + `${goalNameTwo}/`).set({
+            firebase.database().ref('Savings/' + 'JustSave/' + `${user.uid}/` +  `${goalNameTwo}/`).set({
                 amtTwo:amtTwo,
                 goalNameOne:goalNameOne
             })
