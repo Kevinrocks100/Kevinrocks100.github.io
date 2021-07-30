81762af03471db8354ea57f59ef640acdddec689
Paul <35400575+musicEnthusiast03@users.noreply.github.com>
2021-07-29 22:10:33 -0400

    Add files via upload

--- public/budgeting.js
+++ public/budgeting.js
@@ -38,150 +38,101 @@ var firebaseConfig = {
         //         budget:budgetText
         //     })
         // })
-        
+
         document.getElementById("saveOne").addEventListener("click",function(){
-            toIOne = document.getElementById("toIOne").value;
             firebase.database().ref('Budgets/' + 'OfficialBudget/' +`${user.uid}/`).set({
-                toIOne:document.getElementById("toIOne").value,
-                // toPOne:document.getElementById("toPOne").value,
-                // toITwo:document.getElementById("toIOne").value,
-                // toPTwo:document.getElementById("toPOne").value,
-                // toIThree:document.getElementById("toIOne").value,
-                // toPThree:document.getElementById("toPOne").value,
-                // toIFour:document.getElementById("toIOne").value,
-                // toPFour:document.getElementById("toPOne").value
+                [document.getElementById("toIOne").value]:document.getElementById("toPOne").value,
+                [document.getElementById("toITwo").value]:document.getElementById("toPTwo").value,
+                [document.getElementById("toIThree").value]:document.getElementById("toPThree").value,
+                [document.getElementById("toIFour").value]:document.getElementById("toPFour").value
             })
         })
         document.getElementById("saveThree").addEventListener("click",function(){
             firebase.database().ref('Budgets/' + 'OfficialBudget/' +`${user.uid}/`).update({
-                hoIOne:document.getElementById("hoIOne").value,
-                hoPOne:document.getElementById("hoPOne").value,
-                hoITwo:document.getElementById("hoIOne").value,
-                hoPTwo:document.getElementById("hoPOne").value,
-                hoIThree:document.getElementById("hoIOne").value,
-                hoPThree:document.getElementById("hoPOne").value,
-                hoIFour:document.getElementById("hoIOne").value,
-                hoPFour:document.getElementById("hoPOne").value
+                [document.getElementById("hoIOne").value]:document.getElementById("hoPOne").value,
+                [document.getElementById("hoITwo").value]:document.getElementById("hoPTwo").value,
+                [document.getElementById("hoIThree").value]:document.getElementById("hoPThree").value,
+                [document.getElementById("hoIFour").value]:document.getElementById("hoPFour").value
             })
         })
         document.getElementById("saveFour").addEventListener("click",function(){
             firebase.database().ref('Budgets/' + 'OfficialBudget/' + `${user.uid}/`).update({
-                suIOne:document.getElementById("suIOne").value,
-                suPOne:document.getElementById("suPOne").value,
-                suITwo:document.getElementById("suIOne").value,
-                suPTwo:document.getElementById("suPOne").value,
-                suIThree:document.getElementById("suIOne").value,
-                suPThree:document.getElementById("suPOne").value,
-                suIFour:document.getElementById("suIOne").value,
-                suPFour:document.getElementById("suPOne").value
+                [document.getElementById("suIOne").value]:document.getElementById("suPOne").value,
+                [document.getElementById("suITwo").value]:document.getElementById("suPTwo").value,
+                [document.getElementById("suIThree").value]:document.getElementById("suPThree").value,
+                [document.getElementById("suIFour").value]:document.getElementById("suPFour").value
             })
         })
         document.getElementById("saveFive").addEventListener("click",function(){
             firebase.database().ref('Budgets/' + 'OfficialBudget/' + `${user.uid}/`).update({
-                grIOne:document.getElementById("grIOne").value,
-                grPOne:document.getElementById("grPOne").value,
-                grITwo:document.getElementById("grIOne").value,
-                grPTwo:document.getElementById("grPOne").value,
-                grIThree:document.getElementById("grIOne").value,
-                grPThree:document.getElementById("grPOne").value,
-                grIFour:document.getElementById("grIOne").value,
-                grPFour:document.getElementById("grPOne").value
+                [document.getElementById("grIOne").value]:document.getElementById("grPOne").value,
+                [document.getElementById("grITwo").value]:document.getElementById("grPTwo").value,
+                [document.getElementById("grIThree").value]:document.getElementById("grPThree").value,
+                [document.getElementById("grIFour").value]:document.getElementById("grPFour").value
             })
         })
         document.getElementById("saveSix").addEventListener("click",function(){
             firebase.database().ref('Budgets/' + 'OfficialBudget/' + `${user.uid}/`).update({
-                mpIOne:document.getElementById("mpIOne").value,
-                mpPOne:document.getElementById("mpPOne").value,
-                mpITwo:document.getElementById("mpIOne").value,
-                mpPTwo:document.getElementById("mpPOne").value,
-                mpIThree:document.getElementById("mpIOne").value,
-                mpPThree:document.getElementById("mpPOne").value,
-                mpIFour:document.getElementById("mpIOne").value,
-                mpPFour:document.getElementById("mpPOne").value
+                [document.getElementById("mpIOne").value]:document.getElementById("mpPOne").value,
+                [document.getElementById("mpITwo").value]:document.getElementById("mpPTwo").value,
+                [document.getElementById("mpIThree").value]:document.getElementById("mpPThree").value,
+                [document.getElementById("mpIFour").value]:document.getElementById("mpPFour").value
             })
         })
         document.getElementById("saveSeven").addEventListener("click",function(){
             firebase.database().ref('Budgets/' + 'OfficialBudget/' + `${user.uid}/`).update({
-                tpIOne:document.getElementById("tpIOne").value,
-                tpPOne:document.getElementById("tpPOne").value,
-                tpITwo:document.getElementById("tpIOne").value,
-                tpPTwo:document.getElementById("tpPOne").value,
-                tpIThree:document.getElementById("tpIOne").value,
-                tpPThree:document.getElementById("tpPOne").value,
-                tpIFour:document.getElementById("tpIOne").value,
-                tpPFour:document.getElementById("tpPOne").value
+                [document.getElementById("tpIOne").value]:document.getElementById("tpPOne").value,
+                [document.getElementById("tpITwo").value]:document.getElementById("tpPTwo").value,
+                [document.getElementById("tpIThree").value]:document.getElementById("tpPThree").value,
+                [document.getElementById("tpIFour").value]:document.getElementById("tpPFour").value
             })
         })
         document.getElementById("saveEight").addEventListener("click",function(){
             firebase.database().ref('Budgets/' + 'OfficialBudget/' + `${user.uid}/`).update({
-                laIOne:document.getElementById("laIOne").value,
-                laPOne:document.getElementById("laPOne").value,
-                laITwo:document.getElementById("laIOne").value,
-                laPTwo:document.getElementById("laPOne").value,
-                laIThree:document.getElementById("laIOne").value,
-                laPThree:document.getElementById("laPOne").value,
-                laIFour:document.getElementById("laIOne").value,
-                laPFour:document.getElementById("laPOne").value
+                [document.getElementById("laIOne").value]:document.getElementById("laPOne").value,
+                [document.getElementById("laITwo").value]:document.getElementById("laPTwo").value,
+                [document.getElementById("laIThree").value]:document.getElementById("laPThree").value,
+                [document.getElementById("laIFour").value]:document.getElementById("laPFour").value
             })
         })
         document.getElementById("saveNine").addEventListener("click",function(){
             firebase.database().ref('Budgets/' + 'OfficialBudget/' + `${user.uid}/`).update({
-                vhIOne:document.getElementById("vhIOne").value,
-                vhPOne:document.getElementById("vhPOne").value,
-                vhITwo:document.getElementById("vhIOne").value,
-                vhPTwo:document.getElementById("vhPOne").value,
-                vhIThree:document.getElementById("vhIOne").value,
-                vhPThree:document.getElementById("vhPOne").value,
-                vhIFour:document.getElementById("vhIOne").value,
-                vhPFour:document.getElementById("vhPOne").value
+                [document.getElementById("vhIOne").value]:document.getElementById("vhPOne").value,
+                [document.getElementById("vhITwo").value]:document.getElementById("vhPTwo").value,
+                [document.getElementById("vhIThree").value]:document.getElementById("vhPThree").value,
+                [document.getElementById("vhIFour").value]:document.getElementById("vhPFour").value
             })
         })
         document.getElementById("saveTen").addEventListener("click",function(){
             firebase.database().ref('Budgets/' + 'OfficialBudget/' + `${user.uid}/`).update({
-                diIOne:document.getElementById("diIOne").value,
-                diPOne:document.getElementById("diPOne").value,
-                diITwo:document.getElementById("diIOne").value,
-                diPTwo:document.getElementById("diPOne").value,
-                diIThree:document.getElementById("diIOne").value,
-                diPThree:document.getElementById("diPOne").value,
-                diIFour:document.getElementById("diIOne").value,
-                diPFour:document.getElementById("diPOne").value
+                [document.getElementById("diIOne").value]:document.getElementById("diPOne").value,
+                [document.getElementById("diITwo").value]:document.getElementById("diPTwo").value,
+                [document.getElementById("diIThree").value]:document.getElementById("diPThree").value,
+                [document.getElementById("diIFour").value]:document.getElementById("diPFour").value
             })
         })
         document.getElementById("saveEleven").addEventListener("click",function(){
             firebase.database().ref('Budgets/' + 'OfficialBudget/' + `${user.uid}/`).update({
-                ecIOne:document.getElementById("ecIOne").value,
-                ecPOne:document.getElementById("ecPOne").value,
-                ecITwo:document.getElementById("ecIOne").value,
-                ecPTwo:document.getElementById("ecPOne").value,
-                ecIThree:document.getElementById("ecIOne").value,
-                ecPThree:document.getElementById("ecPOne").value,
-                ecIFour:document.getElementById("ecIOne").value,
-                ecPFour:document.getElementById("ecPOne").value
+                [document.getElementById("ecIOne").value]:document.getElementById("ecPOne").value,
+                [document.getElementById("ecITwo").value]:document.getElementById("ecPTwo").value,
+                [document.getElementById("ecIThree").value]:document.getElementById("ecPThree").value,
+                [document.getElementById("ecIFour").value]:document.getElementById("ecPFour").value
             })
         })
         document.getElementById("saveTwelve").addEventListener("click",function(){
             firebase.database().ref('Budgets/' + 'OfficialBudget/' + `${user.uid}/`).update({
-                enIOne:document.getElementById("enIOne").value,
-                enPOne:document.getElementById("enPOne").value,
-                enITwo:document.getElementById("enIOne").value,
-                enPTwo:document.getElementById("enPOne").value,
-                enIThree:document.getElementById("enIOne").value,
-                enPThree:document.getElementById("enPOne").value,
-                enIFour:document.getElementById("enIOne").value,
-                enPFour:document.getElementById("enPOne").value
+                [document.getElementById("enIOne").value]:document.getElementById("enPOne").value,
+                [document.getElementById("enITwo").value]:document.getElementById("enPTwo").value,
+                [document.getElementById("enIThree").value]:document.getElementById("enPThree").value,
+                [document.getElementById("enIFour").value]:document.getElementById("enPFour").value
             })
         })
         document.getElementById("saveThirteen").addEventListener("click",function(){
             firebase.database().ref('Budgets/' + 'OfficialBudget/' + `${user.uid}/`).update({
-                otIOne:document.getElementById("otIOne").value,
-                otPOne:document.getElementById("otPOne").value,
-                otITwo:document.getElementById("otIOne".value),
-                otPTwo:document.getElementById("otPOne").value,
-                otIThree:document.getElementById("otIOne").value,
-                otPThree:document.getElementById("otPOne").value,
-                otIFour:document.getElementById("otIOne").value,
-                otPFour:document.getElementById("otPOne").value
+                [document.getElementById("otIOne").value]:document.getElementById("otPOne").value,
+                [document.getElementById("otITwo").value]:document.getElementById("otPTwo").value,
+                [document.getElementById("otIThree").value]:document.getElementById("otPThree").value,
+                [document.getElementById("otIFour").value]:document.getElementById("otPFour").value
             })
         })
         // saveOne.addEventListener("click", function(){
