50e4bc5f9b43374e66cd38134f0e111cfd62f325
Kevin Liu <liukevin209@gmail.com>
2023-06-01 17:41:37 -0400

    Updated

--- public/login.js
+++ public/login.js
@@ -1,11 +1,11 @@
 var firebaseConfig = {
-  apiKey: "AIzaSyBEXrgqXEA54Jvv1l04Px8bTodK1KAEdAc",
-  authDomain: "fir-f80af.firebaseapp.com",
-  databaseURL: "https://fir-f80af-default-rtdb.firebaseio.com",
-  projectId: "fir-f80af",
-  storageBucket: "fir-f80af.appspot.com",
-  messagingSenderId: "371053966260",
-  appId: "1:371053966260:web:81a6970d6b486bef2138e1"
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  databaseURL: process.env.databaseURL,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
